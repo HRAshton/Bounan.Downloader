b33a520302c74f2b3f35aee1f6a90143f81f4bad
HRAshton <12210721+hrashton@users.noreply.github.com>
2024-03-27 00:24:25 +0100

    Extract services registration to Bootstrap.cs

---
+++ src/Worker/Bootstrap.cs
@@ -0,0 +1,51 @@
+﻿using Amazon.SQS;
+using Bounan.Downloader.Worker.Configuration;
+using Bounan.Downloader.Worker.Factories;
+using Bounan.Downloader.Worker.Interfaces;
+using Bounan.Downloader.Worker.Services;
+using Bounan.LoanApi;
+using Microsoft.Extensions.Options;
+using Telegram.Bot;
+
+namespace Bounan.Downloader.Worker;
+
+public static class Bootstrap
+{
+	public static void RegisterServices(IServiceCollection services, IConfiguration configuration)
+	{
+		ArgumentNullException.ThrowIfNull(services);
+		ArgumentNullException.ThrowIfNull(configuration);
+
+		services.Configure<SqsConfig>(configuration.GetSection(SqsConfig.SectionName));
+		services.Configure<VideoServiceConfig>(configuration.GetSection(VideoServiceConfig.SectionName));
+		services.Configure<TelegramConfig>(configuration.GetSection(TelegramConfig.SectionName));
+
+		services.AddLogging(logging =>
+		{
+			logging.ClearProviders();
+			logging.AddConsole();
+			logging.AddAWSProvider(new AWSLoggerConfigSection(configuration.GetSection(AwsLoggerConfig.SectionName)));
+		});
+
+		Registrar.RegisterServices(services);
+		Registrar.RegisterRefitClients(services);
+
+		services.AddHttpClient();
+
+		services.AddSingleton<IAmazonSQS, AmazonSQSClient>();
+
+		services.AddSingleton<ISqsService, SqsService>();
+		services.AddSingleton<IVideoCopyingService, VideoCopyingService>();
+		services.AddSingleton<IFfmpegFactory, FfmpegFactory>();
+		services.AddTransient<IFfmpegService, FfmpegService>();
+
+		services.AddSingleton<ITelegramBotClient, TelegramBotClient>(provider =>
+		{
+			var telegramConfig = provider.GetRequiredService<IOptions<TelegramConfig>>().Value;
+			var clientOptions = new TelegramBotClientOptions(telegramConfig.BotToken, telegramConfig.ApiUrl.ToString());
+			return new TelegramBotClient(clientOptions);
+		});
+
+		services.AddHostedService<WorkerService>();
+	}
+}
