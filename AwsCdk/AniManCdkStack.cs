0566f73ea6857b8b83e906b4df2352dd02399fa9
HRAshton <12210721+hrashton@users.noreply.github.com>
2024-04-03 18:20:52 +0200

    Add Warmer, StyleCop

--- AwsCdk/AniManCdkStack.cs
+++ AwsCdk/AniManCdkStack.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 using Amazon.CDK;
 using Amazon.CDK.AWS.CloudWatch;
+using Amazon.CDK.AWS.Events;
 using Amazon.CDK.AWS.IAM;
 using Amazon.CDK.AWS.Lambda;
 using Amazon.CDK.AWS.Logs;
@@ -10,95 +12,114 @@ using Amazon.CDK.AWS.SQS;
 using Constructs;
 using Microsoft.Extensions.Configuration;
 using LogGroupProps = Amazon.CDK.AWS.Logs.LogGroupProps;
+using Targets = Amazon.CDK.AWS.Events.Targets;
 
 namespace Bounan.Downloader.AwsCdk;
 
+[SuppressMessage("Naming", "CA1711:Identifiers should not have incorrect suffix", Justification = "This is a stack.")]
 public class AniManCdkStack : Stack
 {
-	internal AniManCdkStack(Construct scope, string id, IStackProps? props = null) : base(scope, id, props)
-	{
-		var config = new ConfigurationBuilder()
-			.AddJsonFile("appsettings.json")
-			.AddEnvironmentVariables()
-			.Build()
-			.Get<BounanCdkStackConfig>();
-		ArgumentNullException.ThrowIfNull(config, nameof(config));
-		config.Validate();
+    internal AniManCdkStack(Construct scope, string id, IStackProps? props = null)
+        : base(scope, id, props)
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables()
+            .Build()
+            .Get<BounanCdkStackConfig>();
+        ArgumentNullException.ThrowIfNull(config, nameof(config));
+        config.Validate();
 
-		var user = new User(this, "User");
+        var user = new User(this, "User");
 
-		GrantPermissionsForQueue(config, user);
-		GrantPermissionsForLambdas(config, user);
+        GrantPermissionsForQueue(config, user);
 
-		var logGroup = CreateLogGroup();
-		SetErrorAlarm(config, logGroup);
-		logGroup.GrantWrite(user);
+        var (getAnimeToDownloadLambda, updateVideoStatusLambda) = GrantPermissionsForLambdas(config);
+        getAnimeToDownloadLambda.GrantInvoke(user);
+        updateVideoStatusLambda.GrantInvoke(user);
 
-		var accessKey = new CfnAccessKey(this, "AccessKey", new CfnAccessKeyProps { UserName = user.UserName });
+        CreateWarmer(config, getAnimeToDownloadLambda);
 
-		Out("Bounan.Downloader.LogGroupName", logGroup.LogGroupName);
-		Out("Bounan.Downloader.UserAccessKeyId", accessKey.Ref);
-		Out("Bounan.Downloader.UserSecretAccessKey", accessKey.AttrSecretAccessKey);
-	}
+        var logGroup = CreateLogGroup();
+        SetErrorAlarm(config, logGroup);
+        logGroup.GrantWrite(user);
 
-	private void GrantPermissionsForQueue(BounanCdkStackConfig config, User user)
-	{
-		var dwnNotificationsQueue = Queue.FromQueueArn(
-			this,
-			"DwnNotificationsSqsQueue",
-			config.NotificationQueueArn);
-		dwnNotificationsQueue.GrantConsumeMessages(user);
-		dwnNotificationsQueue.GrantSendMessages(user);
-	}
+        var accessKey = new CfnAccessKey(this, "AccessKey", new CfnAccessKeyProps { UserName = user.UserName });
 
-	private void GrantPermissionsForLambdas(BounanCdkStackConfig config, User user)
-	{
-		var getAnimeToDownloadLambda = Function.FromFunctionName(
-			this,
-			"LambdaHandlers.GetAnime",
-			config.GetVideoToDownloadLambdaFunctionName);
-		getAnimeToDownloadLambda.GrantInvoke(user);
+        Out("Bounan.Downloader.LogGroupName", logGroup.LogGroupName);
+        Out("Bounan.Downloader.UserAccessKeyId", accessKey.Ref);
+        Out("Bounan.Downloader.UserSecretAccessKey", accessKey.AttrSecretAccessKey);
+    }
 
-		var updateVideoStatusLambda = Function.FromFunctionName(
-			this,
-			"LambdaHandlers.UpdateVideoStatus",
-			config.UpdateVideoStatusLambdaFunctionName);
-		updateVideoStatusLambda.GrantInvoke(user);
-	}
+    private void GrantPermissionsForQueue(BounanCdkStackConfig config, IGrantable user)
+    {
+        var dwnNotificationsQueue = Queue.FromQueueArn(
+            this,
+            "DwnNotificationsSqsQueue",
+            config.NotificationQueueArn);
+        dwnNotificationsQueue.GrantConsumeMessages(user);
+        dwnNotificationsQueue.GrantSendMessages(user);
+    }
 
-	private LogGroup CreateLogGroup()
-	{
-		return new LogGroup(this, "LogGroup", new LogGroupProps
-		{
-			Retention = RetentionDays.ONE_WEEK
-		});
-	}
+    private (IFunction GetAnimeToDownloadLambda, IFunction UpdateVideoStatusLambda) GrantPermissionsForLambdas(
+        BounanCdkStackConfig config)
+    {
+        var getAnimeToDownloadLambda = Function.FromFunctionName(
+            this,
+            "LambdaHandlers.GetAnime",
+            config.GetVideoToDownloadLambdaFunctionName);
 
-	private void SetErrorAlarm(BounanCdkStackConfig bounanCdkStackConfig, ILogGroup logGroup)
-	{
-		var topic = new Topic(this, "LogGroupAlarmSnsTopic", new TopicProps());
+        var updateVideoStatusLambda = Function.FromFunctionName(
+            this,
+            "LambdaHandlers.UpdateVideoStatus",
+            config.UpdateVideoStatusLambdaFunctionName);
 
-		topic.AddSubscription(new EmailSubscription(bounanCdkStackConfig.AlertEmail));
+        return (getAnimeToDownloadLambda, updateVideoStatusLambda);
+    }
 
-		var errorPattern = new MetricFilter(this, "LogGroupErrorPattern", new MetricFilterProps
-		{
-			LogGroup = logGroup,
-			FilterPattern = FilterPattern.AnyTerm("ERROR", "Error", "error"),
-			MetricNamespace = "MetricNamespace",
-			MetricName = "ErrorCount"
-		});
+    private LogGroup CreateLogGroup()
+    {
+        return new LogGroup(this, "LogGroup", new LogGroupProps
+        {
+            Retention = RetentionDays.ONE_WEEK,
+        });
+    }
 
-		_ = new Alarm(this, "LogGroupErrorAlarm", new AlarmProps
-		{
-			Metric = errorPattern.Metric(),
-			Threshold = 1,
-			EvaluationPeriods = 1,
-			TreatMissingData = TreatMissingData.NOT_BREACHING,
-		});
-	}
+    private void SetErrorAlarm(BounanCdkStackConfig bounanCdkStackConfig, ILogGroup logGroup)
+    {
+        var topic = new Topic(this, "LogGroupAlarmSnsTopic", new TopicProps());
 
-	private void Out(string key, string value)
-	{
-		_ = new CfnOutput(this, key, new CfnOutputProps { Value = value });
-	}
+        topic.AddSubscription(new EmailSubscription(bounanCdkStackConfig.AlertEmail));
+
+        var errorPattern = new MetricFilter(this, "LogGroupErrorPattern", new MetricFilterProps
+        {
+            LogGroup = logGroup,
+            FilterPattern = FilterPattern.AnyTerm("ERROR", "Error", "error"),
+            MetricNamespace = "MetricNamespace",
+            MetricName = "ErrorCount",
+        });
+
+        _ = new Alarm(this, "LogGroupErrorAlarm", new AlarmProps
+        {
+            Metric = errorPattern.Metric(),
+            Threshold = 1,
+            EvaluationPeriods = 1,
+            TreatMissingData = TreatMissingData.NOT_BREACHING,
+        });
+    }
+
+    private void CreateWarmer(BounanCdkStackConfig bounanCdkStackConfig, IFunction webhookHandler)
+    {
+        var rule = new Rule(this, "WarmerRule", new RuleProps
+        {
+            Schedule = Schedule.Rate(Duration.Minutes(bounanCdkStackConfig.WarmupTimeoutMinutes)),
+        });
+
+        rule.AddTarget(new Targets.LambdaFunction(webhookHandler));
+    }
+
+    private void Out(string key, string value)
+    {
+        _ = new CfnOutput(this, key, new CfnOutputProps { Value = value });
+    }
 }
